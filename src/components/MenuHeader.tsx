702ba984b3daca335432f39a9598ce35c089a861
Manuel Guevara <dev.smudgy722@passinbox.com>
2025-02-23 00:47:43 -0500

    style: changes color

--- src/components/MenuHeader.tsx
+++ src/components/MenuHeader.tsx
@@ -5,11 +5,12 @@ import { ModeToggle } from "./ThemeToggle";
 export function MenuHeaderMobile({ navItems }: { navItems: any[] }) {
     const [showMenu, setShowMenu] = useState<boolean>(false)
     return (
-        <header className="md:hidden flex flex-col fixed top-0 z-10 w-full py-2 px-3 bg-background/65 backdrop-blur-sm border-b">
-            <div className="flex justify-between">
+        <header className="md:hidden flex flex-col fixed top-0 z-10 w-full py-2 px-3 backdrop-blur bg-background/35">
+            <div className="flex justify-between items-center">
                 <ModeToggle />
+                <p className="font-semibold mr-2">manucode</p>
                 <button
-                    className="mr-2"
+                    className="mr-3"
                     onClick={
                         () => {
                             setShowMenu(!showMenu)
@@ -25,7 +26,7 @@ export function MenuHeaderMobile({ navItems }: { navItems: any[] }) {
                     <ol className="flex flex-col gap-4 w-full pt-4 pb-2">
                         {
                             navItems.map((link) => (
-                                <li >
+                                <li key={link.label}>
                                     <a
                                         className="relative block px-2 py-2 transition hover:text-green-600 dark:hover:text-[#00FF1A]"
                                         aria-label={link.label}
